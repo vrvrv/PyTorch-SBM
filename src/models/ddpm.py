eb04a68ed4155ac2cfa2c91da111309a5a2c3f03
vrvrv <pauljin95@gmail.com>
2021-09-29 13:43:33 +0900

    update

--- src/models/ddpm.py
+++ src/models/ddpm.py
@@ -1,11 +1,17 @@
 # https://github.com/w86763777/pytorch-ddpm/blob/master/main.py
-from typing import List
+from functools import partial
+from typing import List, Optional
+import os
 import copy
 import torch
 import torch.nn.functional as F
+from torchvision.utils import make_grid, save_image
+
 import pytorch_lightning as pl
 from .modules import UNet
-
+from .score import evaluate
+from .sampler import DDPMSampler
+from torchmetrics import MetricCollection, FID, IS
 
 def ema(source, target, decay):
     source_dict = source.state_dict()
@@ -39,6 +45,8 @@ class DDPM(pl.LightningModule):
             beta_T: float,
             warmup: int,
             ema_decay: float,
+            sample_size: int,
+            img_size: int,
             mean_type: str = 'epsilon',
             var_type: str = 'fixedlarge'
     ):
@@ -82,13 +90,38 @@ class DDPM(pl.LightningModule):
             'sqrt_one_minus_alphas_bar', torch.sqrt(1. - alphas_bar)
         )
 
+        self.x_T = torch.randn(sample_size, 3, img_size, img_size)
+
+        self._fid = FID(feature=2048, compute_on_step=True, dist_sync_on_step=True)
+        self._is = IS(feature=2048, compute_on_step=True, dist_sync_on_step=True)
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        # Evaluation
+        # fid_cache = os.path.join(
+        #     self.trainer.default_root_dir, 'stats/fid_cache_train.npz'
+        # )
+        # self.eval_IS_FID = partial(
+        #     evaluate, fid_cache=fid_cache, sample_size=self.hparams.sample_size, img_size=self.hparams.img_size
+        # )
+
+        self.sample_dir = os.makedirs(os.path.join(self.trainer.log_dir, 'sample'), exist_ok=True)
+
+        self.unet_sampler = DDPMSampler(
+            self.unet, beta_1=self.hparams.beta_1, beta_T=self.hparams.beta_T, T=self.hparams.T,
+            mean_type=self.hparams.mean_type, var_type=self.hparams.var_type
+        )
+        self.ema_unet_sampler = DDPMSampler(
+            self.ema_unet, beta_1=self.hparams.beta_1, beta_T=self.hparams.beta_T, T=self.hparams.T,
+            mean_type=self.hparams.mean_type, var_type=self.hparams.var_type
+        )
+
     def forward(self, x_0):
-        t = torch.randint(self.hparams.T, size=(x_0.shape[0], ), device=self.device)
+        t = torch.randint(self.hparams.T, size=(x_0.shape[0],), device=self.device)
 
         noise = torch.randn_like(x_0, device=self.device)
         x_t = (
-            extract(self.sqrt_alphas_bar, t, x_0.shape) * x_0 +
-            extract(self.sqrt_one_minus_alphas_bar, t, x_0.shape) * noise
+                extract(self.sqrt_alphas_bar, t, x_0.shape) * x_0 +
+                extract(self.sqrt_one_minus_alphas_bar, t, x_0.shape) * noise
         )
         loss = F.mse_loss(self.unet(x_t, t), noise, reduction='none')
         return loss
@@ -105,6 +138,27 @@ class DDPM(pl.LightningModule):
         X, y = batch
         loss = self(X).mean()
         self.log("valid_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
+
+    def validation_epoch_end(self, outputs):
+        # Generate image and save
+        x_T = self.x_T.to(self.device)
+
+        x_0 = self.ema_unet_sampler(x_T)
+
+
+        save_image(
+            (make_grid(x_0).cpu() + 1) / 2,
+            fp=os.path.join(self.sample_dir, '%d.png' % self.current_epoch)
+        )
+
+        # Evaluate FID and IS scores
+        ema_fid = self._fid((x_0+1)*255/2)
+        ema_is = self._is((x_0+1)*255/2)
+
+        self.log_dict({
+            'IS_EMA': ema_is,
+            'FID_EMA': ema_fid
+        }, prog_bar=False, logger=True)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=2e-4)
